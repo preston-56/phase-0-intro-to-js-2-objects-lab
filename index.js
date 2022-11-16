7bb9b6f25c17b05b244bfe8e5adebfb7732abc00
preston-56 <prestonosoro56@gmail.com>
2022-11-17 02:41:56 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Write your solution in this file!
+// assigned  an `employee` variable to an `Object` containing `name` and `streetAddress` keys;
+const employee = {
+    name:'Sam',
+    streetAddress:'11 Broadway',
+}
+
+// function takes three arguments and returns an employee with the original key value pairs and the new key value pair
+function updateEmployeeWithKeyAndValue(employee,name,key){
+    const newEmployee={...employee};
+    newEmployee[name]=key;
+    return newEmployee;
+    }
+
+    // updates `employee` with the given `key` and `value` (it is destructive) and returns the entire updated employee
+    function destructivelyUpdateEmployeeWithKeyAndValue(employee,streetAddress,value) {
+        employee[streetAddress] =value;
+        return employee;   
+        
+    }
+// deletes `key` from a clone of employee and returns the new employee
+    function deleteFromEmployeeByKey(employee, name) {
+            const newEmployee={...employee};
+            delete newEmployee.name;
+            return newEmployee;   
+    }
+// returns employee without the deleted key/value pair
+    function destructivelyDeleteFromEmployeeByKey(employee,name){
+        delete employee.name;
+        return employee
+    }
